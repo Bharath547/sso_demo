3e404657fa05c523107ab159c3cd6618afff30ba
Bharath547 <bharathreddy1999d@gmail.com>
2023-02-24 13:50:49 +0000

    added dotnet

---
+++ wwwroot-apis/wwwroot/js/sso-common.js
@@ -0,0 +1,25 @@
+﻿
+// SSO Loader module to show and hide loader
+var SSOLoader = (() => {
+    let showLoader = function () {
+        if (document.getElementById("sso-loader")) {
+            document.getElementById("sso-loader").style.display = 'block';
+        }
+    }
+
+    let hideLoader = function () {
+        if (document.getElementById("sso-loader")) {
+            document.getElementById("sso-loader").style.display = 'none';
+        }
+    }
+
+    return {
+        showLoader: function () {
+            showLoader();
+        },
+         hideLoader: function () {
+            hideLoader();
+        }
+    };
+
+})();
